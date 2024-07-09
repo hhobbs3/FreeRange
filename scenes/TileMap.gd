908afedca9eaee0494f46bfb285fdb482dcd605a
Harlen Hobbs <harlenhobbs3@gmail.com>
2024-07-08 22:09:38 -0400

    minor tilemap edits

--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -23,7 +23,7 @@ func generate_chunk(position):
 			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
 			var temp = temerature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
 			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
-			if alt < 2:
-				set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(0, round(temp+10/5))) # select from specific column (3)
-			else:
-				set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(round(moist+10/5), round(temp+10/5)))
+			# if alt < 2:
+			# 	set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(0, round(temp+10/5))) # select from specific column (3)
+			# else:
+			set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(round(moist+10/5), round(temp+10/5)))
