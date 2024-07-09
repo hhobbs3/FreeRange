9ff7953f151639189c9b44d83609ad95af6acb5d
Harlen Hobbs <harlenhobbs3@gmail.com>
2024-07-08 21:53:03 -0400

    generated platformer test

--- scenes/TileMap.gd
+++ scenes/TileMap.gd
@@ -24,6 +24,6 @@ func generate_chunk(position):
 			var temp = temerature.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
 			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y)*10
 			if alt < 2:
-				set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(13, round(temp+10/5))) # select from specific column (3)
+				set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(0, round(temp+10/5))) # select from specific column (3)
 			else:
 				set_cell(0, Vector2i(tile_pos.x-width/2 + x, tile_pos.y-height/2 + y), 0, Vector2(round(moist+10/5), round(temp+10/5)))
