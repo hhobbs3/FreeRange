f9510942d4feb6e13659565312e274fbc3bb5f62
Harlen Hobbs <harlenhobbs3@gmail.com>
2024-08-10 15:14:18 -0400

    working gun, just a bug with bullets moving after fired

--- scripts/weapons/gun.gd
+++ scripts/weapons/gun.gd
@@ -1,6 +1,6 @@
 extends Node2D
 var bullet = preload('res://scenes/weapons/bullet.tscn')
-@export var bullet_speed = 10
+@export var bullet_speed = 1000
 @onready var gun_sprite_2d = $GunSprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -15,28 +15,20 @@ func _process(_delta):
 		fire()
 		
 func update_facing_direction():
-	
-	var mouse_position = get_global_mouse_position() # + gun_sprite_2d.position
-	var relative_mouse_position = get_global_mouse_position() - gun_sprite_2d.global_position
+	look_at(get_global_mouse_position())
+	var relative_mouse_position = get_global_mouse_position() - global_position
 	# Flip the Gun Sprite
 	if relative_mouse_position.x > 0:
-		gun_sprite_2d.flip_h = false
-		# look_at(relative_mouse_position)
+		gun_sprite_2d.flip_v = false
 	elif relative_mouse_position.x < 0:
-		gun_sprite_2d.flip_h = true
-		# look_at(Vector2(relative_mouse_position.x, -relative_mouse_position.y))
-		# gun_sprite_2d.look_at(-mouse_position)
+		gun_sprite_2d.flip_v = true
 
 func fire():
-	var y = gun_sprite_2d.rotation
-	var x = position - get_global_mouse_position()
 	var bullet_instance = bullet.instantiate()
-	bullet_instance.position = position_offset() # gun_sprite_2d.position
-	bullet_instance.rotation_degrees = rotation_degrees
-	bullet_instance.apply_impulse((-x * bullet_speed).rotated(y))
-	# bullet_instance.apply_impulse(Vector2(bullet_speed, 0).rotated(rotation))
+	bullet_instance.position = position_offset() 
+	bullet_instance.apply_impulse((Vector2(bullet_speed,0)).rotated(rotation))
 	add_child(bullet_instance)
 
 func position_offset():
-	var offset = position #  + Vector2(20,0).rotated(rotation)
+	var offset = position + Vector2(20,0) #  + Vector2(20,0).rotated(rotation)
 	return offset
