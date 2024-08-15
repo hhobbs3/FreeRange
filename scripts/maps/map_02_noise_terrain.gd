bf403522bfef941c4c88c667ad80c1626e218410
Harlen Hobbs <harlenhobbs3@gmail.com>
2024-08-14 21:40:33 -0400

    crab beginnings...

--- scripts/maps/map_02_noise_terrain.gd
+++ scripts/maps/map_02_noise_terrain.gd
@@ -5,8 +5,8 @@ const DIRT = 0
 @onready var noise = $Noise.texture.noise
 
 func _ready():
-	randomize()
-	noise.seed = randi() % 1000
+	#randomize()
+	#noise.seed = randi() % 1000
 	for x in 100:
 		for y in 100:
 			if noise.get_noise_2d(x, y) > 0:
